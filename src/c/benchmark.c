5933e21ffffba79a18e136df45b196a16d7754d5
Rishi Ramraj <rramraj@afilias.info>
2017-01-06 11:54:31 -0500

    The C program sets memory values

--- src/c/benchmark.c
+++ src/c/benchmark.c
@@ -2,9 +2,17 @@
 
 #define LENGTH 10000000
 
-int main()
-{
+int main() {
+  // Allocate the memory.
   int * buffer = malloc(LENGTH * sizeof(int));
+
+  // Seed the memory.
+  for (int index = 0; index < LENGTH; index++) {
+    buffer[index] = index;
+  }
+
+  // Free the memory.
   free (buffer);
+
   return 0;
 }
